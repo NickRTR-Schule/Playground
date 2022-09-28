2c61bb04936c57b464e8ffe9ac18ed0e980772ad
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-09-28 10:34:49 +0200

    complete exercise 1

--- ArrayList/src/benutzerschnittstelle/Konsole.java
+++ ArrayList/src/benutzerschnittstelle/Konsole.java
@@ -1,29 +1,55 @@
 package benutzerschnittstelle;
 
 import java.util.ArrayList;
+import fachkonzept.Person;
 
 public class Konsole
 {
 
 	public static void main(String[] args)
-	{
-		ArrayList<String> names = new ArrayList<>();
+	{		
+		ArrayList<String> namen = new ArrayList<>();
 		ArrayList<Integer> alter = new ArrayList<>();
 		
-		names.add("Julian");
+		namen.add("Julian");
 		alter.add(17);
 		
-		names.add("Nick");
+		namen.add("Nick");
 		alter.add(16);
 		
-		names.add("Lukas");
+		namen.add("Lukas");
 		alter.add(17);
 		
-		for(int i = 0; i < names.size(); i++)
+		for(int i = 0; i < namen.size(); i++)
 		{
-			System.out.println(names.get(i) + " ist " + alter.get(i) + " Jahre alt.");
+			System.out.println(namen.get(i) + " ist " + alter.get(i) + " Jahre alt.");
 		}
 		
+		System.out.println("---------------------------------------------------------");
+		
+		// ArrayList aus Personen
+		
+		ArrayList<Person> personen = new ArrayList<>();
+		
+		personen.add(new Person("Julian", 17));
+		personen.add(new Person("Nick", 16));
+		personen.add(new Person("Lukas", 17));
+		
+		for(int i = 0; i < personen.size(); i++)
+		{
+			System.out.println(personen.get(i).liesName() + " ist " + personen.get(i).liesAlter() + " Jahre alt.");
+		}
+		
+		System.out.println("---------------------------------------------------------");
+		
+		// ArrayList in statischen Array kopieren und ausgeben
+		
+		Person[] statischePersonen = personen.toArray(new Person[personen.size()]);
+		
+		for(int i = 0; i < statischePersonen.length; i++)
+		{
+			System.out.println(statischePersonen[i].liesName() + " ist " + statischePersonen[i].liesAlter() + " Jahre alt.");
+		}
 	}
 
 }
