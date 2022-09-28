36dca7d1171f01011b8792dc89c49db6ab1adcc2
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-09-28 10:12:56 +0200

    Initial commit

---
+++ ArrayList/src/fachkonzept/Person.java
@@ -0,0 +1,23 @@
+package fachkonzept;
+
+public class Person
+{
+	private String name;
+	private int alter;
+	
+	public Person(String pName, int pAlter)
+	{
+		name = pName;
+		alter = pAlter;
+	}
+	
+	public String liesName()
+	{
+		return name;
+	}
+	
+	public int liesAlter()
+	{
+		return alter;
+	}
+}
